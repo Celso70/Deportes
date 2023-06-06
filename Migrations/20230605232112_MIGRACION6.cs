aa0d49c465195b185c2e4f78c8627efb34c80253
Celso70 <crafthell437@gmail.com>
2023-06-05 21:03:34 -0300

    CAMBIOSEZPELETA

---
+++ Migrations/20230605232112_MIGRACION6.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Deportes.Migrations
+{
+    public partial class MIGRACION6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
